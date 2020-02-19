c4aaf7142f92009fa7775ab306cad862cb573e13
Angelus <angelusr@hotmail.fr>
2020-02-19 09:56:10 +0100

    angelus
    J'ai modifié tout le fichier

--- blog-du-siecle/src/App.vue
+++ blog-du-siecle/src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    
-    <Articles/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
-import Articles from './components/Articles.vue'
-
 export default {
-components:{
-  Articles
-}
-}
+  name: "app"
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+h1 {
+  margin-left: 20px;
+  margin-top: 10px;
 }
 </style>
--- blog-du-siecle/src/components/Article.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>{{ article.title }}</div>
-</template>
-
-<script>
-export default {
-    props: ['article']
-};
-</script>
